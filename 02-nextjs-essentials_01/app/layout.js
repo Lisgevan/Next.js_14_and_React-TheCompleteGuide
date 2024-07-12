72a08d5f62e976637b579bf5a8ef669380409622
Lisgevan <episimo@hotmail.com>
2024-07-12 18:59:52 +0300

    Next.js 14 & React - The Complete Guide - 02-nextjs essentials - finished

--- 02-nextjs-essentials_01/app/layout.js
+++ 02-nextjs-essentials_01/app/layout.js
@@ -1,14 +1,19 @@
-import './globals.css'
+import Link from "next/link";
+import "./globals.css";
 
 export const metadata = {
-  title: 'NextJS Course App',
-  description: 'Your first NextJS app!',
+	title: "NextJS Course App",
+	description: "Your first NextJS app!",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<Link href="/">Home</Link>
+				{children}
+			</body>
+		</html>
+	);
 }
+
