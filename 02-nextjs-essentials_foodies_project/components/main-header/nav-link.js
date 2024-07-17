6abcf8e33dd6e5a656fff5b902fdf1054ccf1f52
Lisgevan <episimo@hotmail.com>
2024-07-17 11:41:29 +0300

    Next.js 14 & React - The Complete Guide - 02-nextjs essentials - foodies project - section finished

---
+++ 02-nextjs-essentials_foodies_project/components/main-header/nav-link.js
@@ -0,0 +1,17 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import classes from "./nav-link.module.css";
+
+function NavLink({ href, children }) {
+	const path = usePathname();
+
+	return (
+		<Link className={path.startsWith(href) ? `${classes.link} ${classes.active}` : classes.link} href={href}>
+			{children}
+		</Link>
+	);
+}
+
+export default NavLink;
