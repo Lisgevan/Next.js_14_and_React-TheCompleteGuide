052ac42e902184c313391f5d7acc795251c290f1
Lisgevan <episimo@hotmail.com>
2024-07-20 18:48:01 +0300

    Next.js 14 & React - The Complete Guide - Mutating Data - section finished

--- 06_Mutating_Data/components/posts.js
+++ 06_Mutating_Data/components/posts.js
@@ -1,45 +1,68 @@
-import { formatDate } from '@/lib/format';
-import LikeButton from './like-icon';
+"use client";
 
-function Post({ post }) {
-  return (
-    <article className="post">
-      <div className="post-image">
-        <img src={post.image} alt={post.title} />
-      </div>
-      <div className="post-content">
-        <header>
-          <div>
-            <h2>{post.title}</h2>
-            <p>
-              Shared by {post.userFirstName} on{' '}
-              <time dateTime={post.createdAt}>
-                {formatDate(post.createdAt)}
-              </time>
-            </p>
-          </div>
-          <div>
-            <LikeButton />
-          </div>
-        </header>
-        <p>{post.content}</p>
-      </div>
-    </article>
-  );
+import { useOptimistic } from "react";
+
+import { formatDate } from "@/lib/format";
+import LikeButton from "./like-icon";
+import { togglePostLikeStatus } from "@/actions/posts";
+
+function Post({ post, action }) {
+	return (
+		<article className="post">
+			<div className="post-image">
+				<img src={post.image} alt={post.title} />
+			</div>
+			<div className="post-content">
+				<header>
+					<div>
+						<h2>{post.title}</h2>
+						<p>
+							Shared by {post.userFirstName} on <time dateTime={post.createdAt}>{formatDate(post.createdAt)}</time>
+						</p>
+					</div>
+					<div>
+						<form action={action.bind(null, post.id)} className={post.isLiked ? "liked" : ""}>
+							<LikeButton />
+						</form>
+					</div>
+				</header>
+				<p>{post.content}</p>
+			</div>
+		</article>
+	);
 }
 
 export default function Posts({ posts }) {
-  if (!posts || posts.length === 0) {
-    return <p>There are no posts yet. Maybe start sharing some?</p>;
-  }
+	const [optimisticPosts, updateOptimisticPosts] = useOptimistic(posts, (prevPosts, updatedPostId) => {
+		const updatedPostsIndex = prevPosts.find(post => post.id === updatedPostId);
 
-  return (
-    <ul className="posts">
-      {posts.map((post) => (
-        <li key={post.id}>
-          <Post post={post} />
-        </li>
-      ))}
-    </ul>
-  );
+		if (updatedPostsIndex === -1) return prevPosts;
+
+		const updatedPost = { ...prevPosts[updatedPostsIndex] };
+		updatedPost.likes = updatedPost.likes + (updatedPost.isLiked ? -1 : 1);
+		updatedPost.isLiked = !updatedPost.isLiked;
+		const newPosts = [...prevPosts];
+		newPosts[updatedPostsIndex] = updatedPost;
+		return newPosts;
+	});
+
+	if (!optimisticPosts || posts.length === 0) {
+		return <p>There are no posts yet. Maybe start sharing some?</p>;
+	}
+
+	async function updatePost(postId) {
+		updateOptimisticPosts(postId);
+		await togglePostLikeStatus(postId);
+	}
+
+	return (
+		<ul className="posts">
+			{optimisticPosts.map(post => (
+				<li key={post.id}>
+					<Post post={post} action={updatePost} />
+				</li>
+			))}
+		</ul>
+	);
 }
+
